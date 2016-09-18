2a8641ade97eaf65555f27cfde319473d8630273
sagar-varma <sagar.varma0396@gmail.com>
2016-09-19 00:02:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,6 +73,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/:articleName',function(req,res) {
     var articleName=req.params.articleName;
    res.send(createHtmlTemplate(articles[articleName]));
