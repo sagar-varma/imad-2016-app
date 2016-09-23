eee19c9a9b77360aa1c7442c7397939e15dbdbb8
sagar-varma <sagar.varma0396@gmail.com>
2016-09-23 23:24:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,14 @@
 console.log('Loaded!');
-document.getElementById('demo').innerHTML="hello";
+
+var element=document.getElementById('img1');
+margin=0;
+
+function moveRight() {
+    margin=margin+1;
+    element.style.marginLeft=margin;
+}
+element.onclick=function() {
+    
+    var interval=setInterval(moveRight,50);
+    
+};
