e48c532b3296da1ae7d902de342abcbf7e5dd4cf
sagar-varma <sagar.varma0396@gmail.com>
2016-09-26 22:41:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,34 @@
-console.log('Loaded!');
+var submit=document.getElementById('sub_comment');
 
-var element=document.getElementById('madi');
-margin=0;
-
-function moveRight() {
-    margin=margin+1;
-    element.style.marginLeft=margin;
+submit.onclick=function() {
+    
+    var request =new XMLHttpRequest();
+    
+    request.onreadystatechange = function() {
+        
+        if(request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                
+                var comments=request.responseText;
+                
+                comments=JSON.parse(comments);
+            
+                var list='';
+                
+                for(var i=0;i<comments.length;i++) {
+                    list= "<li>" + comments[i] + "</li>"
+                }
+                
+                var ui=document.getElementById('ul_comment');
+                ui.innerHTML=list;
+            }
+        }
+    }
+    
+    var commentInput=document.getElementById('comment');
+    var comment = commentInput.value;
+    
+    request.open('GET',"http://sagar-varma.imad.hasura-app.io/submit-comment?comment" + comment , true);
+    request.send(null);
+    
 }
-element.onclick=function() {
-    
-    var interval=setInterval(moveRight,50);
-    
-};
